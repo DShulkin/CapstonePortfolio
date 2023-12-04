d023766a8ed98b0cabf8acf0a6b5909d94ab0b7c
David Shulkin <davidshulkin@Davids-iMac.attlocal.net>
2023-12-03 19:05:23 -0800

    finishing mobile homepage

--- js/app.js
+++ js/app.js
@@ -12,42 +12,11 @@ document.querySelectorAll(".nav-link").forEach( nL => nL
         navMenu.classList.remove("active")
     }))
 
-
-    // document.querySelectorAll(".nav-link").onclick = function() {
-    //     hamburger.classList.remove("active")
-    //     navMenu.classList.remove("active")
-    // }​​
-    
-
-
-
-
-
-
-// const homepage = document.querySelector('.homepage')
-// const aboutMe = document.querySelector('.about-me')
-// const portfolio = document.querySelector('.portfolio')
-// const contact = document.querySelector('.contact')
-
-// const body = document.body
-// const navbar = document.createElement("nav")
-// const container = document.createElement("container")
-
-// navbar.classList.add("navbar")
-// body.appendChild(navbar)
-
-// container.classList.add("container")
-// body.appendChild(container)
-
-// const pageSections = {
-//     homepage: document.createElement("div"),
-//     aboutMe: document.createElement("div"),
-//     portfolio: document.createElement("div"),
-//     contact: document.createElement("div")
-// }
-
-// Object.keys(pageSections).forEach(pageSection => {
-//     const element = pageSections[pageSection]
-//     element.classList.add(`${pageSection}`)
-//     body.appendChild(element)
-// })
+    // window.onscroll = function() {
+    //     const navbar = document.querySelector('.navbar');
+    //     if (window.scrollY >= navbar.offsetTop) {
+    //         navbar.classList.add('sticky');
+    //     } else {
+    //         navbar.classList.remove('sticky');
+    //     }
+    // };
